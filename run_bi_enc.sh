bf302e7ba46fce3f21924b65338535c1485a772d
Somin Wadhwa <sominwadhwa@gmail.com>
2020-11-02 18:20:23 -0500

    changed gitignore and ran exp with mantis

--- run_bi_enc.sh
+++ run_bi_enc.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 #
 #SBATCH --job-name=bi_enc
-#SBATCH --output=logs/run_bi_enc_%j.txt  # output file
-#SBATCH -e logs/run_bi_enc_%j.err        # File to which STDERR will be written
 #SBATCH --gres=gpu:1
 #SBATCH --partition=titanx-long # Partition to submit to
 #SBATCH --mem=50000
-#
 #SBATCH --ntasks=1
+#SBATCH --time=02-01:00:00
 
-
-#Training a Bi-Encoder using cross entropy loss:
-python ./main_bert_bi.py -- --dataset ubuntu_data --batch 16 --nmodel BiEncoderDot2 --epochs 1 
+python3 ./main_bert_bi.py -- --dataset mantis --batch 16 --nmodel BiEncoderDot2 --epochs 1 
--- run_cross_enc.sh
+++ run_cross_enc.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 #
 #SBATCH --job-name=cross_enc
-#SBATCH --output=logs/run_cross_enc_%j.txt  # output file
-#SBATCH -e logs/run_cross_enc_%j.err        # File to which STDERR will be written
 #SBATCH --gres=gpu:1
-#SBATCH --partition=1080ti-long # Partition to submit to
+#SBATCH --partition=titanx-long # Partition to submit to
 #SBATCH --mem=50000
-#
 #SBATCH --ntasks=1
+#SBATCH --time=02-01:00:00
 
-
-#Training an enchanced Cross-Encoder (BECA) using cross entropy loss:
-python ./main_bert_bi.py -- --dataset ubuntu_data --batch 8 --nmodel MolyEncoderAggP2 --epochs 1
+python3 ./main_bert_bi.py -- --dataset mantis --batch 8 --nmodel MolyEncoderAggP2 --epochs 1
