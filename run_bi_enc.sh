7840b68a3ba5070a048a892cc028f1f6f9883f15
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-11-04 01:39:47 -0500

    remove copy, make args work

--- run_bi_enc.sh
+++ run_bi_enc.sh
@@ -2,8 +2,11 @@
 #
 #SBATCH --job-name=bi_enc
 #SBATCH --gres=gpu:1
+#SBATCH --output=logs/run_bi_enc_%j.txt  # output file
+#SBATCH -e logs/run_bi_enc_%j.err        # File to which STDERR will be written
 #SBATCH --partition=1080ti-long # Partition to submit to
 #SBATCH --mem=50000
 #SBATCH --ntasks=1
 
-python3 ./main_bert_bi.py --dataset=mantis --batch=16 --nmodel=BiEncoderDot2 --epochs 1 
+python3 ./main_bert_bi.py --dataset=ubuntu_data --batch=16 --nmodel=BiEncoderDot2 --epochs 1 
+python3 ./main_bert_bi.py --dataset=mantis --batch=16 --nmodel=BiEncoderDot2 --epochs 1
--- run_cross_enc.sh
+++ run_cross_enc.sh
@@ -2,8 +2,11 @@
 #
 #SBATCH --job-name=cross_enc
 #SBATCH --gres=gpu:1
+#SBATCH --output=logs/run_cross_enc_%j.txt  # output file
+#SBATCH -e logs/run_cross_enc_%j.err        # File to which STDERR will be written
 #SBATCH --partition=2080ti-long # Partition to submit to
 #SBATCH --mem=50000
 #SBATCH --ntasks=1
 
-python3 ./main_bert_bi_copy.py --dataset=mantis --batch=8 --nmodel=MolyEncoderAggP2 --epochs 1
+python3 ./main_bert_bi.py --dataset=ubuntu_data --batch=8 --nmodel=MolyEncoderAggP2 --epochs 1
+#python3 ./main_bert_bi.py --dataset=mantis --batch=8 --nmodel=MolyEncoderAggP2 --epochs 1
